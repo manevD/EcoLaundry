8dc6a0ff9ed62a72247429de688c1b450a6b9a1c
Dimitar Manev <dimitar.manev@outlook.de>
2026-07-02 23:13:25 +0200

    redirect to login

--- Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -31,7 +31,7 @@ public class LogoutModel : PageModel
         {
             // This needs to be a redirect so that the browser performs a new
             // request and the identity for the user gets updated.
-            return RedirectToPage();
+            return Redirect("/Identity/Account/Login");
         }
     }
 }
